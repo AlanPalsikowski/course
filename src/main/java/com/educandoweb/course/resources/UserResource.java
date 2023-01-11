a04824f5250620ece38ccde422b4d3174a6d56b8
Alan <alanpalsikowski@gmail.com>
2023-01-11 12:05:46 -0300

    created resources e entities

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//oi
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
