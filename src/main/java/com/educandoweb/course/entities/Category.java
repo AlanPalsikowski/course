5a16ff8f973020394d06302becc12394ec37976b
Alan <alanpalsikowski@gmail.com>
2023-01-17 11:36:39 -0300

    product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -3,20 +3,28 @@ package com.educandoweb.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
-@Entity
-@Table (name= "tb_category")
-public class Category  implements Serializable {
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
-    public Category (){
+  //  @JsonIgnore
+   // @ManyToMany(mappedBy = "categories")
+   // private Set<Product> products = new HashSet<>();
+
+    public Category() {
     }
 
     public Category(Long id, String name) {
+        super();
         this.id = id;
         this.name = name;
     }
@@ -36,6 +44,10 @@ public class Category  implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+   // public Set<Product> getProducts() {
+    //    return products;
+  //  }
 
     @Override
     public int hashCode() {
